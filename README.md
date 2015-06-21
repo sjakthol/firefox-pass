bf790a5253871e190857b5c5b367da556f01edd3
Sami Jaktholm <sjakthol@outlook.com>
2015-06-21 11:49:27 +0300

    Remove the selector dialog and instead always copy the password for the currently active site.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Firefox Pass
-An add-on for accessing the [pass](http://www.passwordstore.org/) password manager directly from Firefox. It adds an context menu item to password inputs that can be used to retrieve a password from the pass store. Once the correct password is selected, it will be copied to clipboard for 45 seconds (using `pass --clip`) during which it can be pasted to a password field.
+An add-on for accessing the [pass](http://www.passwordstore.org/) password manager directly from Firefox. It adds an context menu item to password inputs that can be used to retrieve a password from the pass store. The password will be copied to clipboard for 45 seconds (using `pass --clip`) during which it can be pasted to a password field.
 
 # Requirements
 The `pass` script needs to be installed and initialized. The add-on can be built using the `jpm` utility (`npm install -g jpm`).
--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 "use strict";
 
+const _ = require("sdk/l10n").get;
+const { notify } = require("sdk/notifications");
 const Pass = require("src/pass");
 
 Pass.isAvailable().then(() => {
-  const _ = require("sdk/l10n").get;
+  return Pass.listPasswords();
+}).then(sites => {
   const cm = require("sdk/context-menu");
+  const { URL } = require("sdk/url");
+  const { clearTimeout, setTimeout } = require("sdk/timers");
 
-  const panel = require("src/panel");
+  let hostnamePredicate = context => {
+    let host = URL(context.documentURL).host;
+    if (!host || typeof host !== "string" || !host.length) {
+      return false;
+    }
 
-  // Add a context menu item for password inputs.
+    return !!sites.find(site => site.endsWith(host));
+  };
+
+  // Show the menu item if it opens on a password input and the URL of the
+  // frame is stored in the manager.
+  let context = [cm.SelectorContext("input[type='password']"),
+                 cm.PredicateContext(hostnamePredicate)];
+
   cm.Item({
     label: _("ui.ctx.label"),
-    context: cm.SelectorContext("input[type='password']"),
-    contentScript: "self.on('click', self.postMessage)",
-    onMessage: ev => {
-      panel.show();
+    context: context,
+    contentScript: "self.on('click', _ => self.postMessage(_.ownerDocument.URL))",
+    onMessage: url => {
+      let host = URL(url).host;
+      let site = sites.find(site => site.endsWith(host));
+
+      // Show the success notification on a timeout as a successful copy is
+      // blocks the pass command for 45 seconds.
+      let successNotificationTimeout = setTimeout(() => {
+        notify({
+          title: _("ui.notification.success.title"),
+          text: _("ui.notification.success.body")
+        });
+      }, 500);
+
+      Pass.copyToClipboard(site).catch(err => {
+        // Cancel the success notification.
+        clearTimeout(successNotificationTimeout);
+        notify({
+          title: _("ui.notification.error.title"),
+          text: _("ui.notification.error.body")
+        });
+      });
     }
   });
-}, stderr => {
-  console.error("pass is not available. Add-on functionality disabled.");
+}).catch(err => {
+  console.log(err);
+  notify({
+    title: _("ui.notification.initFail.title"),
+    text: _("ui.notification.initFail.body")
+  });
 });
